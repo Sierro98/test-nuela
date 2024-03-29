07a5dbf26bed4e4d86e24fa4e0661650f4e404de
Alejandro Sierro <121672722+Sierro98@users.noreply.github.com>
2024-03-29 15:04:03 +0100

    Improve code reusability

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import SideBar from "./components/SideBar"
+import SideBar from "./components/SideBar";
 
 function App() {
-
   return (
-   <div><SideBar/></div>
-    
-  )
+    <div className="container-fluid">
+      <div className="row flex-nowrap">
+        <SideBar/>
+        <div className="col py-3 bg-light">Content area...</div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
