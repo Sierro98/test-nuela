6ba3974f7f9ed56a4842f44ec952ff92bcea4bfa
Alejandro Sierro <121672722+Sierro98@users.noreply.github.com>
2024-03-30 19:35:27 +0100

    Added the button and changed the logo and the title

--- src/components/ui-elements/AssignmentTable.tsx
+++ src/components/ui-elements/AssignmentTable.tsx
@@ -1,20 +1,24 @@
-
 import TableRadioButton from "./TableRadioButton";
 
 const AssignmentTable: React.FC = () => {
-    return (
-        <div className="d-flex flex-column gap-4">
-            <TableRadioButton
-              name="HourGroup"
-              options={[
-                { label: "Horas lectivas", value: "1" },
-                { label: "Horas complementarias", value: "2" },
-              ]}
-              onChange={(value) => console.log(value)}
-              defaultValue="1"
-             />
-        </div>
-    );
-}
+  return (
+    <div className="d-flex flex-column gap-5">
+      <TableRadioButton
+        name="HourGroup"
+        options={[
+          { label: "Horas lectivas", value: "1" },
+          { label: "Horas complementarias", value: "2" },
+        ]}
+        onChange={(value) => console.log(value)}
+        defaultValue="1"
+      />
+      <div className="d-flex justify-content-end">
+        <button type="button" className="btn btn-primary w-25">
+          + Añadir Asignatura
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default AssignmentTable;
