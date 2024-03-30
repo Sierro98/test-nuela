a09ce183fd82f3e4aa6baf04231fdea384d85bb9
Alejandro Sierro <121672722+Sierro98@users.noreply.github.com>
2024-03-30 20:49:22 +0100

    Advancements on the modal

--- src/components/ui-elements/AssignmentTable.tsx
+++ src/components/ui-elements/AssignmentTable.tsx
@@ -33,7 +33,7 @@ const AssignmentTable: React.FC = () => {
           <div className="modal-content">
             <div className="modal-header">
               <h1 className="modal-title fs-5" id="staticBackdropLabel">
-                Modal title
+                Añadir asignatura
               </h1>
               <button
                 type="button"
@@ -44,11 +44,8 @@ const AssignmentTable: React.FC = () => {
             </div>
             <div className="modal-body">...</div>
             <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
-                Close
-              </button>
               <button type="button" className="btn btn-primary">
-                Understood
+                + Añadir asignatura
               </button>
             </div>
           </div>
